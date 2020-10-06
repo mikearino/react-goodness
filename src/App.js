6f571f86cc3357ba8526d303969014da2eafa66f
Mike Lofgren <mikeylofgren@gmail.com>
2020-10-06 10:25:13 -0700

    default props/destructuring.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ function App() {
   return (
     <div className="App">
       <h1>I like turtles</h1>
-      <OtherComponent />
-      <OtherComponent />
+      <OtherComponent name="Mike" greeting="Yo" />
+      <OtherComponent name="PepPep" />
     </div>
   );
 }
 
-function OtherComponent() {
-  return <h1>Yep</h1>;
+function OtherComponent({ name, greeting = 'Hello' }) {
+  return (
+    <h1>
+      {' '}
+      {greeting} {name}
+    </h1>
+  );
 }
 
 export default App;
