5fc6cf436b7c9d5af42d40e9cf11e70c07a6077a
Mike Lofgren <mikeylofgren@gmail.com>
2020-10-07 10:34:14 -0700

    add in different conditional examples.

--- src/Accordian.js
+++ src/Accordian.js
@@ -1,11 +1,28 @@
+// My example
+// import React, { useState } from 'react';
+
+// export function Accordian() {
+//   const [visible, setVisible] = useState(true);
+//   return (
+//     <div>
+//       {visible ? <h3>Show Me</h3> : null}
+//       <button onClick={() => setVisible(false)}>Toggle</button>
+//     </div>
+//   );
+// }
+
 import React, { useState } from 'react';
 
 export function Accordian() {
-  const [visible, setVisible] = useState(true);
+  const [isToggled, setIsToggled] = useState(false);
+  const showMe = isToggled ? <h3>Show Me</h3> : null;
   return (
     <div>
-      {visible ? <h3>Show Me</h3> : null}
-      <button onClick={() => setVisible(false)}>Toggle</button>
+      {showMe}
+      {/* {isToggled && <h3>Show Me</h3>} */}
+      {/* {isToggled ? <h3>Show Me</h3> : null} */}
+
+      <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
     </div>
   );
 }
