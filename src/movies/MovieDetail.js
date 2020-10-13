19d861f2797aac6030f90bdbf5f1cc8197eb8b1e
Mike Lofgren <mikeylofgren@gmail.com>
2020-10-13 10:11:46 -0700

    add conditional rendering for movie details.

--- src/movies/MovieDetail.js
+++ src/movies/MovieDetail.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+
+const API_KEY = '?api_key=941007440b9046ef5468ea86902a8290';
 
 export function MovieDetail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState({});
+
+  const getMovie = async () => {
+    try {
+      const res = await fetch(BASE_URL + id + API_KEY);
+      const newMovie = await res.json();
+      setMovie(newMovie);
+      console.log('movie', newMovie);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  //Any time id changes, fetch information
+  useEffect(() => {
+    getMovie();
+  }, [id]);
+
+  if (!movie.title) return null;
   return (
     <div>
-      <h1>Movie Detail {id}</h1>
+      <h1>{movie.title}</h1>
+      <p>{movie.overview}</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
